5342acb08eca4ea91fa8c263b96551c21dbeb4f4
MijinKo <kasoutuukamoon55@gmail.com>
2018-03-28 21:11:07 +0900

    見た目いろいろ

--- app/src/main/java/com/daddypool/dpm/MainActivity.java
+++ app/src/main/java/com/daddypool/dpm/MainActivity.java
@@ -2,6 +2,8 @@ package com.daddypool.dpm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView  myWebView = (WebView)findViewById(R.id.webView);
+        //標準ブラウザをキャンセル
+        myWebView.setWebViewClient(new WebViewClient());
+        //アプリ起動時に読み込むURL
+        myWebView.loadUrl("http://daddy-pool.work/");
     }
 }

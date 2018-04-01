d55ea1f3d181982a0ef21ccfb850652f4d916b53
OKField <japanesta05@gmai.com>
2018-04-01 09:20:25 +0900

    アドレスの記憶

--- app/src/main/java/com/daddypool/dpm/MainActivity.java
+++ app/src/main/java/com/daddypool/dpm/MainActivity.java
@@ -1,12 +1,25 @@
 package com.daddypool.dpm;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView textView;
+    private EditText editText;
+    private String fileName = "Address.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +29,81 @@ public class MainActivity extends AppCompatActivity {
         myWebView.setWebViewClient(new WebViewClient());
         //アプリ起動時に読み込むURL
         myWebView.loadUrl("http://daddy-pool.work/api/stats");
+
+
+
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+
+        //保存してあるアドレスがあれば読み込んで表示する
+        String str = readFile(fileName);
+        if (str != null) {
+            textView.setText(str);
+        } else {
+            textView.setText(R.string.read_error);
+        }
+
+        Button buttonSave = findViewById(R.id.button);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // エディットテキストのテキストを取得
+                String text = editText.getText().toString();
+
+                saveFile(fileName, text);
+                if(text.length() == 0){
+                    textView.setText(R.string.no_text);
+                }
+                else{
+                    textView.setText(R.string.saved);
+                }
+                String str = readFile(fileName);
+                if (str != null) {
+                    textView.setText(str);
+                } else {
+                    textView.setText(R.string.read_error);
+                }
+
+            }
+        });
     }
+
+    // ファイルを保存
+    public void saveFile(String file, String str) {
+
+        // try-with-resources
+        try (FileOutputStream fileOutputstream = openFileOutput(file,
+                Context.MODE_PRIVATE);){
+
+            fileOutputstream.write(str.getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    // ファイルを読み出し
+    public String readFile(String file) {
+        String text = null;
+
+        // try-with-resources
+        try (FileInputStream fileInputStream = openFileInput(file);
+             BufferedReader reader= new BufferedReader(
+                     new InputStreamReader(fileInputStream,"UTF-8"));
+        ) {
+
+            String lineBuffer;
+            while( (lineBuffer = reader.readLine()) != null ) {
+                text = lineBuffer ;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text;
+    }
+
+
 }

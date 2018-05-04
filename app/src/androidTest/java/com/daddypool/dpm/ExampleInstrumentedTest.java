dc263effe059c6bb8bc402e953fbacc12d40f2ef
jankendo <jankendo@icloud.com>
2018-05-04 19:35:14 +0900

    icon

--- app/src/androidTest/java/com/daddypool/dpm/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/daddypool/dpm/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.daddypool.dpm", appContext.getPackageName());
+        assertEquals("com.ic_launcher.dpm", appContext.getPackageName());
     }
 }

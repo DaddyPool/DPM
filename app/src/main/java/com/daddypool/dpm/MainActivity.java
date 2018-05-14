d91f5b55e44e1e7386555648e2ece838e714814a
MijinKo <kasoutuukamoon55@gmail.com>
2018-05-14 23:07:45 +0900

    グラフの簡易実装（参考サイトをそのまま移植）

--- app/src/main/java/com/daddypool/dpm/MainActivity.java
+++ app/src/main/java/com/daddypool/dpm/MainActivity.java
@@ -40,6 +40,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+//>>グラフ追加
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+//<<グラフ追加
+
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<JSONObject> {
     private TextView textView;
     private TextView textDiffData;
@@ -55,6 +72,8 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     private EditText editText;
     private String fileName = "Address.txt";
     private String text ="";
+    private LineChart mChart;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -138,7 +157,40 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
             }
         });
+        //>>グラフ追加
+        mChart = findViewById(R.id.line_chart);
 
+        // Grid背景色
+        mChart.setDrawGridBackground(true);
+
+        // no description text
+        mChart.getDescription().setEnabled(true);
+
+        // Grid縦軸を破線
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.enableGridDashedLine(10f, 10f, 0f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis leftAxis = mChart.getAxisLeft();
+        // Y軸最大最小設定
+        leftAxis.setAxisMaximum(150f);
+        leftAxis.setAxisMinimum(0f);
+        // Grid横軸を破線
+        leftAxis.enableGridDashedLine(10f, 10f, 0f);
+        leftAxis.setDrawZeroLine(true);
+
+        // 右側の目盛り
+        mChart.getAxisRight().setEnabled(false);
+
+        // add data
+        setData();
+
+        mChart.animateX(2500);
+        //mChart.invalidate();
+
+        // dont forget to refresh the drawing
+        // mChart.invalidate();
+        //<<グラフ追加
     }
 
     // ファイルを保存
@@ -252,5 +304,55 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         //PoolStats 用
         getLoaderManager().restartLoader(2, null, this);
     }
+    //グラフ追加
+    private void setData() {
+        // Entry()を使ってLineDataSetに設定できる形に変更してarrayを新しく作成
+        int data[] = {116, 111, 112, 121, 102, 83,
+                99, 101, 74, 105, 120, 112,
+                109, 102, 107, 93, 82, 99, 110,
+        };
 
+        ArrayList<Entry> values = new ArrayList<>();
+
+        for (int i = 0; i < data.length; i++) {
+            values.add(new Entry(i, data[i], null, null));
+        }
+
+        LineDataSet set1;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+
+            set1 = (LineDataSet) mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet");
+
+            set1.setDrawIcons(false);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(0f);
+            set1.setDrawFilled(true);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+            set1.setFillColor(Color.BLUE);
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData lineData = new LineData(dataSets);
+
+            // set data
+            mChart.setData(lineData);
+        }
+    }
 }

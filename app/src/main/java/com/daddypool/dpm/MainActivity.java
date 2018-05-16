afe0ff210f7e44ed7243884785c14881d08a0f74
MijinKo <kasoutuukamoon55@gmail.com>
2018-05-16 22:01:34 +0900

    スプラッシュの仮実装

--- app/src/main/java/com/daddypool/dpm/MainActivity.java
+++ app/src/main/java/com/daddypool/dpm/MainActivity.java
@@ -79,6 +79,13 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // ここで1秒間スリープし、スプラッシュを表示させたままにする。
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+        }
+        // スプラッシュthemeを通常themeに変更する
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         // ツールバーをアクションバーとしてセット
